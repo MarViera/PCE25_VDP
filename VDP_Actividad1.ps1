06660d71f468873babde82c03e4b8f0ea6d07600
valeriaduronp <valeria.duronpns@uanl.edu.mx>
2025-05-23 19:31:23 -0600

    Add files via upload
    Script de powershell, modificado para que python lo ejecute. Tuve que eliminar los write-host que estaban porque se me mezclaban en la lista de las IPs

---
+++ VDP_Actividad1.ps1
@@ -0,0 +1,16 @@
+﻿# Obtener todas las conexiones de red activas
+$connections = Get-NetTCPConnection | Select-Object -ExpandProperty RemoteAddress
+
+# Filtrar para eliminar valores vacíos y evitar repetir IPs
+$uniqueIPs = $connections | Where-Object { $_ -match "\d+\.\d+\.\d+\.\d+" } | Sort-Object -Unique
+
+
+# Opcional: Guardar la lista en un archivo de texto
+$uniqueIPs | Out-File -FilePath "ConexionesIPs.txt"
+
+
+#Convertir $uniqueIPs a una lista separada con comas (para que python pueda leer todas las IPs sin problema)
+Write-Output ($uniqueIPs -join ",")
+
+
+#Elimine los write-host ya que para ejecutar el script en la actividad 2 se me mezclaba con las IP y agregue write-output para que python lo pueda leer facilmente"""
